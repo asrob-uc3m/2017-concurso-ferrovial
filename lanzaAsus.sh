58b8ae1b1d949f185230589a9f17dea86ce760f1
jgvictores <jcgvicto@gmail.com>
2017-02-02 15:18:36 +0100

    only one asus

--- lanzaAsus.sh
+++ lanzaAsus.sh
@@ -1,2 +1,2 @@
 yarpdev --device OpenNI2DeviceServer --noRGBMirror --noDepthMirror # 320x240 - 30 fps
-yarpdev --device OpenNI2DeviceServer --noRGBMirror --noDepthMirror --depthVideoMode 4 --colorVideoMode 9  # 640x480 - 30 fps
+####yarpdev --device OpenNI2DeviceServer --noRGBMirror --noDepthMirror --depthVideoMode 4 --colorVideoMode 9  # 640x480 - 30 fps
