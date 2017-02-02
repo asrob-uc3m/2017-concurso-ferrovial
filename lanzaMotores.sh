a7b73962f365fae38be70833da95bbcf7b0e480a
jgvictores <jcgvicto@gmail.com>
2017-02-02 19:53:41 +0100

    new

--- lanzaMotores.sh
+++ lanzaMotores.sh
@@ -1,1 +1,4 @@
-yarpdev --device EcroWheelController --serialPortName /dev/ttyUSB0
+sudo chmod 777 /dev/ttyACM0
+yarpdev --device EcroPwmController --serialPortName /dev/ttyACM0
+
+### old: yarpdev --device EcroWheelController --serialPortName /dev/ttyUSB0
