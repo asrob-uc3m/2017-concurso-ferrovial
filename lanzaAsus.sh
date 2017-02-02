7335bf108b6c32fb1425f2f4df430e48f7ffafc4
jgvictores <jcgvicto@gmail.com>
2017-02-02 15:15:46 +0100

    improve OpenNI2DeviceServer

--- lanzaAsus.sh
+++ lanzaAsus.sh
@@ -1,1 +1,2 @@
-yarpdev --device OpenNI2DeviceServer --depthVideoMode 4 --colorVideoMode 9 --noRGBMirror --noDepthMirror
+yarpdev --device OpenNI2DeviceServer --noRGBMirror --noDepthMirror # 320x240 - 30 fps
+yarpdev --device OpenNI2DeviceServer --noRGBMirror --noDepthMirror --depthVideoMode 4 --colorVideoMode 9  # 640x480 - 30 fps
