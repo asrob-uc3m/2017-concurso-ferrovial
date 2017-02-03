2de080791a93866c93f90b3bf05f31f30ae067ba
jgvictores <jcgvicto@gmail.com>
2017-02-03 11:58:37 +0100

    varsfor height and width

--- programs/depth.py
+++ programs/depth.py
@@ -5,6 +5,9 @@ import numpy
 
 import matplotlib.pylab
 
+yarp_width = 320
+yarp_height = 240
+
 yarp.Network.init()
 
 input_port = yarp.Port()
@@ -17,9 +20,9 @@ if yarp.Network.connect("/OpenNI2/depthFrame:o", "/python/depth:i") != True:
 #while(1):
 
 # Create numpy array to receive the image and the YARP image wrapped around it
-img_array = numpy.zeros((240, 320), dtype=numpy.uint16)
+img_array = numpy.zeros(( yarp_height , yarp_width ), dtype=numpy.uint16)
 yarp_image = yarp.ImageMono16()
-yarp_image.resize(320, 240)
+yarp_image.resize( yarp_width , yarp_height )
 yarp_image.setExternal(img_array, img_array.shape[1], img_array.shape[0])
 # Read the data from the port into the image
 input_port.read(yarp_image)
